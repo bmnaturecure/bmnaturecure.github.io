3375d59ff249e55e5184e38215184efd0fa822d2
Hersh Amin <h.amin@utexas.edu>
2016-05-25 16:43:25 +0530

    Added flickr feed in gallery and twitter feed in blog

--- js/gallery.js
+++ js/gallery.js
@@ -1,38 +1,36 @@
 // Controller for gallery
 
 var app = angular.module('bmn', []);
-app.controller('galleryCtrl', function($scope) {
-    // TEMP Gallery code
-    $scope.photos = [
-        {
-            'image' : 'img/portfolio/1.jpg',
-            'title' : 'Title 1',
-            'description' : 'Description 1'
-        },
-        {
-            'image' : 'img/portfolio/2.jpg',
-            'title' : 'Title 2',
-            'description' : 'Description 2'
-        },
-        {
-            'image' : 'img/portfolio/3.jpg',
-            'title' : 'Title 3',
-            'description' : 'Description 3'
-        },
-        {
-            'image' : 'img/portfolio/4.jpg',
-            'title' : 'Title 4',
-            'description' : 'Description 4'
-        },
-        {
-            'image' : 'img/portfolio/5.jpg',
-            'title' : 'Title 5',
-            'description' : 'Description 5'
-        },
-        {
-            'image' : 'img/portfolio/6.jpg',
-            'title' : 'Title 6',
-            'description' : 'Description 6'
+app.controller('galleryCtrl', function($scope, $http, $window) {
+    
+    // Constants
+    var baseUrl = "https://api.flickr.com/services/rest/";
+    var flickrApiKey = "a782fbcbe110b9c024f0c6003b447dfe";
+    var flickrPhotosetId = "72157668865244715";
+    var flickrUserId = "143475214@N04";
+    var flickrMethod = "flickr.photosets.getphotos";
+    
+    // Variables
+    $scope.photos = [];
+    
+    var photoUrl = baseUrl + "?method=" + flickrMethod + "&api_key=" + flickrApiKey + "&photoset_id=" + flickrPhotosetId + "&user_id=" + flickrUserId + "&extras=url_m,description&format=json&nojsoncallback=?"
+    
+    // Get photos
+    $http.get(photoUrl).then(function successRes(response) {
+        
+        // Got data
+        var photos = response.data.photoset.photo;
+        for (var i=0; i<photos.length; i++) {
+            $scope.photos.push({
+                'image' : photos[i].url_m,
+                'title' : photos[i].title,
+                'description' : photos[i].description._content
+            });
         }
-    ]
+        
+    }, function errorRes(response) {
+        // Error
+        $window.alert(response.statusText);
+    });
+    
 });
